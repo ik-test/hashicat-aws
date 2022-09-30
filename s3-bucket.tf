99c71968cbeddd67356898d7e37fce3f92fb1480
ik-test <ignacykat@gmail.com>
2022-09-30 17:12:14 +0000

    Added s3 bucket module ok

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,31 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
+module "s3-bucket" {
+  source  = "app.terraform.io/iguana-company/s3-bucket/aws"
+  version = "2.8.0"
+ 
+bucket = "my-s3-bucket"
   acl    = "private"
 
   versioning = {
     enabled = true
   }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
