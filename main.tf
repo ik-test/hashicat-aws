0fa8be50280fbfc6e3ef6bc0d84171df6c91203a
ik-test <ignacykat@gmail.com>
2022-09-30 12:44:31 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -127,7 +127,9 @@ resource "aws_instance" "hashicat" {
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
   tags = {
-    Name = "${var.prefix}-hashicat-instance"
+    Environment = "Department"
+    Name = "devops"
+    value = "devops"
   }
 }
 
