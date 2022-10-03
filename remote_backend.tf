b2914a072edcca7627a510c414ff0cea0ce994ac
ik-test <ignacykat@gmail.com>
2022-10-03 12:00:02 +0000

    Added remote backendd

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
@@ -7,3 +8,14 @@ terraform {
     }
   }
 }
+=======
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "iguana-company"
+    workspaces {
+      name = "hashicat-aws"
+    }
+  }
+}
+>>>>>>> 99c71968cbeddd67356898d7e37fce3f92fb1480
